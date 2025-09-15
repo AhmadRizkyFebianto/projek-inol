5e1005c61cb2edf9fd960dd29d6c1edb2fc92cf3
shrmanto <shrmantoch@gmail.com>
2025-09-16 06:35:57 +0700

    feat: navbar & footer component

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Link, TabNav, Text } from "@radix-ui/themes";
+import Logo from "../assets/logo.png";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Flex justify="between" align="center" px="29px" py="10px">
+        <img src={Logo} width="50" height="40" />
+        <Flex align="center" gap="5">
+          <Link href="#" underline="none" color="gray" highContrast>
+            Chatbot
+          </Link>
+          <Link href="#" underline="none" color="gray" highContrast>
+            Beli Rumah
+          </Link>
+          <Link href="#" underline="none" color="gray" highContrast>
+            Hitung KPR
+          </Link>
+          <Link href="#" underline="none" color="gray" highContrast>
+            Masuk
+          </Link>
+          <div className="bg-black w-[1.5px] h-7" />
+          <Link href="#" underline="none" color="gray" highContrast>
+            Daftar
+          </Link>
+        </Flex>
+      </Flex>
+    </nav>
+  );
+}
