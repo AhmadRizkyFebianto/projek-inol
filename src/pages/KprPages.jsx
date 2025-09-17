d7653604db9a522778e6b5a84863a7421baaa21c
Ahmad Rizky Febianto <ahmadrizky13jkl@gmail.com>
2025-09-18 04:11:47 +0700

    feat (kpr) : update view & responsive

--- src/pages/KprPages.jsx
+++ src/pages/KprPages.jsx
@@ -20,22 +20,32 @@ export default function KprPage() {
                 key={i}
                 className="w-full overflow-hidden bg-white rounded-lg shadow-md"
               >
-                <div className="flex items-center justify-center h-32 bg-gray-200">
-                  <span className="text-gray-400">Image</span>
-                </div>
-                <div className="p-3">
-                  <h3 className="font-semibold">Perumahan Griya</h3>
-                  <p className="text-sm text-gray-500">Jakarta Timur</p>
-                  <p className="text-xs text-gray-500">
-                    LT 97m² | LB 78m² | L1
-                  </p>
-                  <div className="flex items-center justify-between mt-2">
-                    <span className="px-2 py-1 font-bold text-black bg-yellow-400 rounded">
-                      Rp 2.589.500
-                    </span>
-                    <button className="text-xs text-gray-500 hover:underline">
-                      Transaksi
-                    </button>
+                <div className="max-w-xs rounded-xl overflow-hidden shadow-lg">
+                  {/* Image */}
+                  <div className="w-full h-[200px] bg-gray-300 flex items-center justify-center">
+                    <span className="text-gray-400">Image</span>
+                  </div>
+
+                  {/* Content */}
+                  <div className="flex items-center justify-between bg-gray-100 p-3">
+                    {/* Info kiri */}
+                    <div>
+                      <h3 className="text-lg font-semibold text-gray-800">
+                        Perumahan Griya
+                      </h3>
+                      <p className="text-sm text-gray-700">Jakarta Timur</p>
+                      <p className="text-xs text-gray-400 mt-1">
+                        LT 97m² | LB 78m² | L1
+                      </p>
+                    </div>
+
+                    {/* Info kanan */}
+                    <div className="text-right">
+                      <span className="block text-base font-semibold text-gray-800 bg-yellow-400 px-3 rounded-lg">
+                        Rp 2.589.500
+                      </span>
+                      <p className="text-xs text-gray-600 mt-1">Transaksi</p>
+                    </div>
                   </div>
                 </div>
               </div>
