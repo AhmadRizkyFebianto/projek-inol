84e129fc9937bf2034f6949f9fc81bf52b348930
Ahmad Rizky Febianto <ahmadrizky13jkl@gmail.com>
2025-09-17 23:44:53 +0700

    update

--- src/pages/KprPages.jsx
+++ src/pages/KprPages.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "../component/Sidebar";
-import Navbar from "../component/navbar";
+import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
 
 export default function KprPage() {
